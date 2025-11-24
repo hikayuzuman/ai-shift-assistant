3751f0e9f887d653c542b7d4e7d2c105b2197819
hikayuzuman <hikathemea@gmail.com>
2025-11-24 15:41:24 +0900

    Delete page.tsx

--- page.tsx
+++
@@ -1,9 +0,0 @@
-// app/page.tsx または pages/index.tsx
-export default function Home() {
-  return (
-    <div style={{ padding: 50, fontSize: 24 }}>
-      <h1>AIシフトアシスタント</h1>
-      <p>デプロイ成功！✨</p>
-    </div>
-  );
-}
