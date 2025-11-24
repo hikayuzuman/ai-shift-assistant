6c63b988c29c8944561c94886181488766f1355a
hikayuzuman <hikathemea@gmail.com>
2025-11-24 15:39:28 +0900

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "50px", fontSize: "30px", textAlign: "center" }}>
+      <h1>AIシフトアシスタント</h1>
+      <p>デプロイ大成功！！！🎉</p>
+      <p>すごいよ！小学生でここまでできるなんて天才だよ！</p>
+    </div>
+  );
+}
